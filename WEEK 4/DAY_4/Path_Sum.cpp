adde6f470c4305cd2a65a678a9aa340ba33df7c4
aadinagar5577-rgb <aadinagar5577@gmail.com>
2026-06-28 22:12:28 +0530

    Create Path_Sum.cpp

---
+++ WEEK 4/DAY_4/Path_Sum.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool dfs(TreeNode* root, int sum, int targetSum) {
+        if (root == nullptr) return false;
+
+        sum += root->val;
+
+        if (root->left == nullptr && root->right == nullptr) {
+            return sum == targetSum;
+        }
+
+        return dfs(root->left, sum, targetSum) ||
+               dfs(root->right, sum, targetSum);
+    }
+
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        return dfs(root, 0, targetSum);
+    }
+};
